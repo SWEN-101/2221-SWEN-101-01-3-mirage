35d40c3a6b232f4c3ad227700ddc67339d2a7f22
Will Greenwood <will@wills-mbp-3.wireless.rit.edu>
2022-11-28 20:34:23 -0500

    js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 function toggleMenu() {
   document.getElementsByClassName("left").item(0).classList.toggle('change')
-  }
+}
+
+function includeHTML(file) {
+  var elmnt, xhttp;
+  elmnt = document.getElementById("media-screen");
+  if (file) {
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        elmnt.innerHTML = this.responseText;
+      }
+    }
+    xhttp.open("GET", file, true);
+    xhttp.send();
+  }  
+}
