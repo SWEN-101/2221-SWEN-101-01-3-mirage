e307152933261b4f5950a00c334c0c8dc37ac39c
Will Greenwood <will@wills-mbp-3.wireless.rit.edu>
2022-11-28 20:48:41 -0500

    rip onload

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
 function toggleMenu() {
   document.getElementsByClassName("left").item(0).classList.toggle('change')
 }
-
-function includeHTML(elmnt, file) {
-  var xhttp;
-  if (file) {
-    xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        elmnt.innerHTML = this.responseText;
-      }
-    }
-    xhttp.open("GET", file, true);
-    xhttp.send();
-  }  
-}
